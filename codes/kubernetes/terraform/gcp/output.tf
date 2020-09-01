09cc8d1522171f9a8316dfd82e52a512df4bc3b8
Yuri <liangy3928@gmail.com>
2020-09-01 21:18:33 +0800

    add bastion output

--- codes/kubernetes/terraform/gcp/output.tf
+++ codes/kubernetes/terraform/gcp/output.tf
@@ -1,3 +1,7 @@
+output "bastion_public_ip" {
+  value = google_compute_instance.bastion.network_interface.0.nat_ip
+}
+
 output "controller_private_ip" {
   value = google_compute_instance.controller.*.network_interface.0.network_ip
 }
@@ -5,3 +9,4 @@ output "controller_private_ip" {
 output "worker_private_ip" {
   value = google_compute_instance.worker.*.network_interface.0.network_ip
 }
+
--- codes/output.tf
+++ codes/output.tf
@@ -1,3 +1,7 @@
+output "bastion" {
+  value = module.kubernetes.bastion_public_ip
+}
+
 output "controller_nodes" {
   value = module.kubernetes.controller_private_ip
 }
